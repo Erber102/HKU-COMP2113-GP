040c386310078d19097f9ece746e07851473c8ff
Wenyi0606 <u3629114@connect.hku.hk>
2025-11-26 16:33:30 +0800

    Update Config.h

--- src/Core/Config.h
+++ src/Core/Config.h
@@ -1,4 +1,3 @@
-
 #ifndef CONFIG_H
 #define CONFIG_H
 
@@ -14,21 +13,21 @@ namespace EasyConfig {
     const int STARTING_HP = 120;
     const int STARTING_STAMINA = 70;
     const int STARTING_MONEY = 50;
-    const float DEMAND_GROWTH_RATE = 1.03f;
+    const double DEMAND_GROWTH_RATE = 1.03;
 }
 
 namespace NormalConfig {
     const int STARTING_HP = 100;
     const int STARTING_STAMINA = 50;
     const int STARTING_MONEY = 0;
-    const float DEMAND_GROWTH_RATE = 1.05f;
+    const double DEMAND_GROWTH_RATE = 1.05;
 }
 
 namespace HardConfig {
     const int STARTING_HP = 80;
     const int STARTING_STAMINA = 30;
     const int STARTING_MONEY = -20; // 初始负债
-    const float DEMAND_GROWTH_RATE = 1.08f;
+    const double DEMAND_GROWTH_RATE = 1.08;
 }
 
 // 存档相关配置

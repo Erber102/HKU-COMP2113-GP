79679c14a9095da41e9bb169489d88099d954280
Timlang567 <timlang567@gmail.com>
2025-11-28 18:49:44 +0800

    Update comment to English in combat_simple.cpp

--- src/Combat/combat_simple.cpp
+++ src/Combat/combat_simple.cpp
@@ -15,7 +15,7 @@ void Combat::fightEnemy(int enemyIndex) {
     cout << "=== Battle Start ===" << endl;
     cout << "Fighting enemy... (simplified version)" << endl;
 
-    // 简化的战斗逻辑
+    // simplified combat system logic
     cout << "Player attacks enemy!" << endl;
     cout << "Enemy takes damage!" << endl;
     cout << "Enemy defeated!" << endl;
